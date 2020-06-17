d92823ce9c7c0e363e40a3847083ec7ee6d14d5d
nquangtoan98 <45713942+nquangtoan98@users.noreply.github.com>
2020-06-17 22:20:23 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("sidebar");
+    if (x.className === "menu-left border") {
+        x.className += " active";
+    } else {
+        x.className = "menu-left border";
+    }
+
+    var els = document.querySelectorAll("#lv2");
+    for (var i = 0; i < els.length; i++) {
+		 if (els[i].className === "list-lv2") {
+	        els[i].classList.remove("list-lv2");
+	    } else {
+	        els[i].className += "list-lv2";
+	    }
+	}
+
+	var main1 = document.getElementById("main1");
+	if (main1.className === "main1 border") {
+        main1.className += " main1res";
+    } else {
+        main1.className = "main1 border";
+    }
+}

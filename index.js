ae0c165ce4003522d99b33d9eeca5c92540cb096
nquangtoan98 <45713942+nquangtoan98@users.noreply.github.com>
2020-06-18 14:08:06 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -27,12 +27,11 @@ function minimizeFunction(){
     var icon = document.getElementById("icon");
     var els = document.querySelectorAll("#lv2");
     for (var i = 0; i < els.length; i++) {
-         if (els[i].className === "list-lv2") {
-            els[i].classList += " minimize";
-            icon.classList = "fa fa-plus";
-        } else {
-            els[i].className = "list-lv2";
+        els[i].classList.toggle("minimize");    
+        if (els[i].className === "list-lv2") {
             icon.classList = "fa fa-minus";
+        } else {
+            icon.classList = "fa fa-plus";
         }  
     }
 }
